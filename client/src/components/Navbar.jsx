064ceaf8463ffaa97f8946b061b92d929ba8476b
Rangel Shishkov <rshishkov01@gmail.com>
2025-01-14 23:35:55 +0200

    imagekit setup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,46 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
+import { IKImage } from "imagekitio-react";
+import Image from "./Image";
 
 const Navbar = () => {
-    const [open,setOpen] = useState(false)
-    return (
-      <div className="w-full h-16 md:h-20 flex items-center justify-between">
-        {/* LOGO */}
-        <div className="flex gap-4 items-center text-2xl font-bold">
-          <img src="/logo.png" className="h-12 w-12" alt="site-logo" />
-          <span>HighFive</span>
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex gap-4 items-center text-2xl font-bold">
+        <Image src="logo.png" alt="site logo" w={56} h={56} />
+        <span>HighFive</span>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        {/* BUTTON */}
+        <div
+          className="cursor-pointer text-2xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "x" : "≡"}
         </div>
-        {/* MOBILE MENU */}
-        <div className="md:hidden">
-          {/* BUTTON */}
-          <div
-            className="cursor-pointer text-2xl"
-            onClick={() => setOpen((prev) => !prev)}
-          >
-            {open ? "x" : "≡"}
-          </div>
-          <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out
-            ${open ? "-right-0" : "-right-[100%]"}`}>
-            <a href="/">Home</a>
-            <a href="/">Trending</a>
-            <a href="/">Most Popular</a>
-            <a href="/">About</a>
-            <a href="">
-                <button className="py-2 px-4 rounded-3xl bg-red-600">Login</button>
-            </a>
-          </div>    
-        </div>
-        {/* DESKTOP MENU */}
-        <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-            <a href="/">Home</a>
-            <a href="/">Trending</a>
-            <a href="/">Most Popular</a>
-            <a href="/">About</a>
-            <a href="">
-                <button className="py-2 px-4 rounded-3xl bg-red-600">Login</button>
-            </a>
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out
+            ${open ? "-right-0" : "-right-[100%]"}`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-red-600">Login</button>
+          </a>
         </div>
       </div>
-    );
-}
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-red-600">Login</button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
