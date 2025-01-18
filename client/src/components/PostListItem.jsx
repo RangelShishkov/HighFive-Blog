f22005e17bc8a816ee131b8829fa9aa555ffb879
Rangel Shishkov <rshishkov01@gmail.com>
2025-01-18 14:54:51 +0200

    create PostList, PostListItem components

---
+++ client/src/components/PostListItem.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import Image from "./Image";
+
+const PostListItem = () => {
+  return (
+    <div className="flex flex-col xl:flex-row gap-8">
+      {/* Image */}
+      <div className="md:hidden xl:block xl:w-1/3">
+        <Image src="tyrant.jpg" className="rounded-2xl object-cover" />
+      </div>
+      {/* Details */}
+      <div className="flex flex-col gap-4 xl:w-2/3">
+        <Link to="/test" className="text-4xl font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, cum.
+        </Link>
+        <div className="flex items-center gap-2 text-gray-800 text-sm">
+          <span>Written by</span>
+          <Link className="text-red-600">John Doe</Link>
+          <span>on</span>
+          <Link className="text-red-600">Web Design</Link>
+          <span>2 days ago</span>
+        </div>
+        {/* Description */}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vitae possimus voluptates ducimus cumque similique, non nihil quia culpa ullam doloribus eaque cum esse veritatis officiis et eveniet, consectetur eligendi?</p>
+        <Link to="test" className="text-sm underline text-red-600">Read More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostListItem;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import MainCategories from "../components/MainCategories";
 import FeaturedPosts from "../components/FeaturedPosts";
+import PostList from "../components/PostList";
 
 const Home = () => {
   return (
@@ -57,6 +58,13 @@ const Home = () => {
       <MainCategories />
       {/* POSTS */}
       <FeaturedPosts />
+      {/* RECENT POSTS */}
+      <div>
+        <h1 className="my-8 text-2xl text-gray-800">
+          Recent Posts
+        </h1>
+        <PostList />
+      </div>
     </div>
   );
 };
