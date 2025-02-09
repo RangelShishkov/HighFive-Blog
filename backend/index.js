f8cafee6783f6e8d20b18d152fd7fbaaa3f067f7
Rangel Shishkov <rshishkov01@gmail.com>
2025-02-09 15:15:00 +0200

    post CRUD setup

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,21 @@
 import express from "express";
-import connectDB from './lib/connectDB.js'
-import userRouter from "./routes/user.route.js"
-import postRouter from "./routes/post.route.js"
-import commentRouter from "./routes/comment.route.js"
+import connectDB from "./lib/connectDB.js";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 
 const app = express();
+app.use(express.json());
 
-app.get("/home", (req,res) => {
-  res.status(200).send("Hello!")
-})
+app.get("/home", (req, res) => {
+  res.status(200).send("Hello!");
+});
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
 app.listen(3030, () => {
-  connectDB()
+  connectDB();
   console.log("server is listening!");
 });
