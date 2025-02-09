c1047d0344a3bd77f9c29d70727cceb21d45a29f
Rangel Shishkov <rshishkov01@gmail.com>
2025-02-09 15:22:44 +0200

    Express 5 Error Handling

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,15 @@ app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message || "Something went wrong!",
+    status: error.status,
+    stack: error.stack,
+  });
+});
+
 app.listen(3030, () => {
   connectDB();
   console.log("server is listening!");
